115db8c9766ca2cca88e8762d67fd9bfa0ab2302
Kartikey_Pandey1884 <51755548+kartikey-onlineGOD@users.noreply.github.com>
2022-11-05 22:32:39 -0400

    New changes
    22:32

--- src/answer.py
+++ src/answer.py
@@ -82,8 +82,9 @@ def response():
     model.compile(loss='categorical_crossentropy', optimizer= sgd, metrics= 
     ['accuracy'])
 
-    model.fit(np.array(train_x),np.array(train_y),epochs=5000,batch_size=5,
+    hist = model.fit(np.array(train_x),np.array(train_y),epochs=5000,batch_size=5,
     verbose=1)
+    model.save("discordbot.h5",hist)
     print("done")
 
 
