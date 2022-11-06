415d27dfdcfc517b042afdd0e8de0f092a1dba7b
Kartikey_Pandey1884 <51755548+kartikey-onlineGOD@users.noreply.github.com>
2022-11-05 20:41:33 -0400

    Latest Answers

--- src/answer.py
+++ src/answer.py
@@ -10,12 +10,11 @@ def response():
     
     import nltk 
     from nltk.stem import WordNetLemmatizer
+    lemmatizer = nltk.stem.WordNetLemmatizer()
 
     from tensorflow.keras.models import Sequential 
     from tensorflow.keras.layers import Dense, Activation, Dropout 
     from tensorflow.keras.optimizers import SGD
-
-    lemmatizer = WordNetLemmatizer
 
     intents = json.loads(open("assets\intents.json").read())
 
@@ -33,8 +32,8 @@ def response():
             if intent['tag'] not in classes: 
                 classes.append(intent['tag'])
 
+    words = [lemmatizer.lemmatize(word.lower()) for word in words if word not in ignore_letters]
 
-    words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
     words = sorted(set(words))
 
 
@@ -54,9 +53,10 @@ def response():
     for document in documents: 
         bag = []
         word_patterns = document[0]
-        word_patterns[lemmatizer.lemmatize(word.lower() for word in word_patterns)]
+        for word in word_patterns:
+            word_patterns = [lemmatizer.lemmatize(word.lower())]
         for word in words:
-            bag.append(1) if word in word_patterns else bag.appends(0) 
+            bag.append(1) if word in word_patterns else bag.append(0) 
 
         output_row = list(output_empty)
         output_row[classes.index(document[1])] = 1
@@ -69,22 +69,22 @@ def response():
     train_x = list(training[:,0])
     train_y = list(training[:,1])
 
-    model = Sequential()
-    model.add(Dense(128,input_shape =len(train_x[0]),),activation = 'relu')
-    model.add(Dropout(0.5))
-    model.add(Dense(64,activation = 'relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(len(train_y[0]),actiavtion ="softmax"))
 
-    sgd = SGD(lr = 0.01,decay = 1e-6, momentum = 0.9, nesterov = True)
-    model.compile(loss = "categorial_crossentropy",    optimizer = sgd, metrics = ['accuracy'])
+    model=Sequential()
+    model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+    model.add(Dropout(0.5))
 
-    model.fit(np.array(train_x),np.array(train_y),epochs = 200,batch_size = 5, verbose = 1)
-    model.save('discordbot.model')
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(len(train_y[0]), activation='softmax'))
+    sgd= SGD(lr=0.01, decay=1e-6, momentum=0.9,nesterov=True)
+    model.compile(loss='categorical_crossentropy', optimizer= sgd, metrics= 
+    ['accuracy'])
+
+    model.fit(np.array(train_x),np.array(train_y),epochs=5000,batch_size=5,
+    verbose=1)
     print("done")
-
-
-
 
 
 
